4ca8e9f4d0b60ea81c94812b4d785f930898fd9e
bgl-20 <thebackyardgolfleague@gmail.com>
2026-04-12 20:08:17 -0400

    made a middlewear to have netlify pick up shop.backyard golf and be able to use style

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+
+export function middleware(request: NextRequest) {
+  const host = request.headers.get("host") || ""
+  
+  if (host === "shop.backyardgolf.io") {
+    const url = request.nextUrl.clone()
+    if (url.pathname === "/") {
+      url.pathname = "/shop"
+      return NextResponse.rewrite(url)
+    }
+  }
+  
+  return NextResponse.next()
+}
